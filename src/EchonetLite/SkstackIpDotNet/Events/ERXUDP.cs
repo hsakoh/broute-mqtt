e65f488d2a01ae525c00a03e808316e80addbf9d
hsakoh <20980395+hsakoh@users.noreply.github.com>
2026-04-02 02:31:02 +0900

    Fix handling of ERXUDP events with different lengths when SA2 flag is enabled on BP35C0

--- src/EchonetLite/SkstackIpDotNet/Events/ERXUDP.cs
+++ src/EchonetLite/SkstackIpDotNet/Events/ERXUDP.cs
@@ -12,20 +12,42 @@
         public ERXUDP(string response, bool isBP35C0) : base(response)
         {
             var values = response.Split(' ');
-            Sender = values[1];
-            Dest = values[2];
-            RPort = values[3];
-            LPort = values[4];
-            SenderLla = values[5];
-            Secured = values[6];
             if (isBP35C0)
             {
-                Side = values[7];
-                DataLen = values[8];
-                Data = values[9];
+                if (values.Length == 10)
+                {
+                    Sender = values[1];
+                    Dest = values[2];
+                    RPort = values[3];
+                    LPort = values[4];
+                    SenderLla = values[5];
+                    Secured = values[6];
+                    Side = values[7];
+                    DataLen = values[8];
+                    Data = values[9];
+                }
+                else if (values.Length == 11)
+                {
+                    Sender = values[1];
+                    Dest = values[2];
+                    RPort = values[3];
+                    LPort = values[4];
+                    SenderLla = values[5];
+                    RSSI = values[6];
+                    Secured = values[7];
+                    Side = values[8];
+                    DataLen = values[9];
+                    Data = values[10];
+                }
             }
             else
             {
+                Sender = values[1];
+                Dest = values[2];
+                RPort = values[3];
+                LPort = values[4];
+                SenderLla = values[5];
+                Secured = values[6];
                 DataLen = values[7];
                 Data = values[8];
             }
@@ -51,6 +73,10 @@
         /// 送信元の MAC 層アドレス(64bit)
         /// </summary>
         public string SenderLla { get; set; }
+        /// <summary>
+        /// 受信した UDP を構成する最後の MAC フレームの受信 RSSI レベル（SA2 レジスタ=1 の場合に表示されます）
+        /// </summary>
+        public string RSSI { get; set; }
         /// <summary>
         /// 1:受信した IP パケットを構成する MAC フレームが暗号化されていた場合
         /// 0: 受信した IP パケットを構成する MAC フレームが暗号化されていなかった場合
